c7bb5a51c3482c43739bbbd711a21e21f0b2da08
zhouwei1997 <hengyu19971023@163.com>
2023-03-23 18:48:13 +0800

    验证码-短信验证码测试

--- meiduo_mall/apps/verifications/tests.py
+++ meiduo_mall/apps/verifications/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
-
 # Create your tests here.
+from ronglian_sms_sdk import SmsSDK
+
+from verifications import constants
+
+
+def send_message():
+    sdk = SmsSDK(constants.ACCOUNT_SID, constants.AUTH_TOKEN, constants.APP_ID)
+    tid = '1'
+    mobile = '15027130472'
+    datas = ('123456', '5分钟')
+    resp = sdk.sendMessage(tid, mobile, datas)
+    print(resp)
+
+
+send_message()
