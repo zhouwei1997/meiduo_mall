68e31569ceca4816c70bb55b2c896beed9bbe373
zhouwei1997 <hengyu19971023@163.com>
2023-03-23 14:48:27 +0800

    短信验证码token等常量文件-配合容联云

--- meiduo_mall/apps/verifications/constants.py
+++ meiduo_mall/apps/verifications/constants.py
@@ -3,7 +3,7 @@
 # @author ZhouWei
 # @date  2023/3/22
 # @file  constants.py
-# @description  常量文件
+# @description  验证码使用的常量 文件
 """
 # 图形验证码有效时间  单位：秒
 IMAGE_CODE_REDIS_EXPIRES = 300
@@ -13,3 +13,9 @@ SMS_CODE_REDIS_EXPIRES = 300
 SEND_SMS_TEMPLATE_ID = 1
 # 60s 内是否重发标记
 SEND_SMS_CODE_INTERVAL = 60
+# 容联云
+ACCOUNT_SID = '2c94887686c00d75018709a1f74110c8'
+# meiduo_mall子应用的token
+AUTH_TOKEN = '811971d5df5634ec054e12718fad0132'
+# meiduo_mall的 APP_ID
+APP_ID = '2c94887686c00d75018709a551af10d3'
