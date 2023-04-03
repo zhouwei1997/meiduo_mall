177479e6f7f462491a780731e6d639196dcc3dbe
zhouwei1997 <hengyu19971023@163.com>
2023-04-03 11:51:30 +0800

    更新readme

--- meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/apps/verifications/views.py
@@ -111,10 +111,6 @@ class SMSCodeView(View):
         # 生成短信验证码
         sms_code = '%06d' % random.randint(0, 999999)
         logger.info(sms_code)
-        # # 保存短信验证码
-        # redis_conn.setex('sms_%s' % mobile, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
-        # # 保存发送短信标记
-        # redis_conn.setex('send_flag_%s' % mobile, constants.SEND_SMS_CODE_INTERVAL, 1)
         # 创建redis的管道
         pl = redis_conn.pipline()
         # 将命令添加到管道中
