8bba1c4be88c0f256aae35a33296deb44caf3a92
zhouwei1997 <hengyu19971023@163.com>
2023-04-05 17:28:00 +0800

    celery-定义发送短信任务

--- celery_tasks/main.py
+++ celery_tasks/main.py
@@ -11,3 +11,5 @@ from celery import Celery
 celery_app = Celery('meiduo')
 # 加载celery配置文件
 celery_app.config_from_object('celery_tasks.config')
+# 注册任务
+celery_app.autodiscover_tasks(['celery_tasks.sms'])
--- meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/apps/verifications/views.py
@@ -1,15 +1,13 @@
 # Create your views here.
-import json
 import logging
 import random
 
 from django import http
 from django.views import View
 from django_redis import get_redis_connection
-from ronglian_sms_sdk import SmsSDK
 
+from celery_tasks.sms import constants
 from meiduo_mall.utils.response_code import RETCODE
-from verifications import constants
 from verifications.libs.captcha.captcha import captcha
 
 # 创建日志输出器
@@ -36,37 +34,37 @@ class ImageCodeView(View):
         return http.HttpResponse(image, content_type='image/jpg')
 
 
-class CCP(object):
-    """发送短信验证码的单例类"""
-
-    def __new__(cls, *args, **kwargs):
-        """
-        定义单例的初始化方法
-        判断单例是否存在，如果存在，初始化。返回单例
-        :param args:
-        :param kwargs:
-        :return 单例
-        """
-        # _instance 属性中存储的就是单例
-        if not hasattr(cls, '_instance'):
-            cls._instance = super(CCP, cls).__new__(cls, *args, **kwargs)
-            # 初始化单例
-            cls._instance.sdk = SmsSDK(constants.ACCOUNT_SID, constants.AUTH_TOKEN, constants.APP_ID)
-        return cls._instance
-
-    def send_template(self, tid, mobile, datas):
-        """
-        发送短信验证码单例方法
-        :param datas: 内容数据
-        :param tid: 模板ID
-        :param mobile: 手机号
-        :return: 成功 0  失败 -1
-        """
-        response = json.loads(self.sdk.sendMessage(tid, mobile, datas))
-        if response.get('statusCode') == '000000':
-            return 0
-        else:
-            return -1
+# class CCP(object):
+#     """发送短信验证码的单例类"""
+#
+#     def __new__(cls, *args, **kwargs):
+#         """
+#         定义单例的初始化方法
+#         判断单例是否存在，如果存在，初始化。返回单例
+#         :param args:
+#         :param kwargs:
+#         :return 单例
+#         """
+#         # _instance 属性中存储的就是单例
+#         if not hasattr(cls, '_instance'):
+#             cls._instance = super(CCP, cls).__new__(cls, *args, **kwargs)
+#             # 初始化单例
+#             cls._instance.sdk = SmsSDK(constants.ACCOUNT_SID, constants.AUTH_TOKEN, constants.APP_ID)
+#         return cls._instance
+#
+#     def send_template(self, tid, mobile, datas):
+#         """
+#         发送短信验证码单例方法
+#         :param datas: 内容数据
+#         :param tid: 模板ID
+#         :param mobile: 手机号
+#         :return: 成功 0  失败 -1
+#         """
+#         response = json.loads(self.sdk.sendMessage(tid, mobile, datas))
+#         if response.get('statusCode') == '000000':
+#             return 0
+#         else:
+#             return -1
 
 
 class SMSCodeView(View):
@@ -120,7 +118,7 @@ class SMSCodeView(View):
         # 执行
         pl.execute()
         # 发送短信验证码
-        CCP().send_template(constants.SEND_SMS_TEMPLATE_ID, mobile, [sms_code, constants.SMS_CODE_REDIS_EXPIRES // 60])
+        # CCP().send_template(constants.SEND_SMS_TEMPLATE_ID, mobile, [sms_code, constants.SMS_CODE_REDIS_EXPIRES // 60])
         # 响应结果
         return http.JsonResponse({
             'code': RETCODE.IMAGECODEERR,
