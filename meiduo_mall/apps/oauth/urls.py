9521698754e12b0088d03338f0ef5722e9dfa5c9
zhouwei1997 <hengyu19971023@163.com>
2023-04-12 19:07:01 +0800

    绑定oauth的url

---
+++ meiduo_mall/apps/oauth/urls.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+"""
+# @author ZhouWei
+# @date  2023/4/12
+# @file  urls.py
+# @description
+"""
+from django.conf.urls import url
+
+from oauth import views
+
+urlpatterns = [
+    # 提供QQ登录扫码页面
+    url(r'^qq/login/$', views.QQAuthURLView.as_view()),
+]
