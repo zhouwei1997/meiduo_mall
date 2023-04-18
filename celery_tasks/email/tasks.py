e9adf43d12a58f3297f6cd895e3a0f532c3369ac
zhouwei1997 <hengyu19971023@163.com>
2023-04-18 22:03:45 +0800

    celery-发送邮箱-错误重试机制

--- celery_tasks/email/tasks.py
+++ celery_tasks/email/tasks.py
@@ -15,10 +15,15 @@ from celery_tasks.main import celery_app
 logger = logging.getLogger('django')
 
 
-@celery_app.task(name='send_verify_email')
-def send_verify_email(to_email, verify_url):
+# bind：保证task对象会作为第一个参数自动传入
+# name：异步任务别名
+# retry_backoff：异常自动重试的时间间隔，第n次(retry_backoff * 2^(n-1))s
+# max_retries：异常自动重试次数的上限
+@celery_app.task(bind=True, name='send_verify_email', retry_backoff=3)
+def send_verify_email(self, to_email, verify_url):
     """
     定义发送邮件任务
+    :param self:
     :param to_email: 收件人
     :param verify_url: 激活连接
     :return:
@@ -29,4 +34,9 @@ def send_verify_email(to_email, verify_url):
                    '<p>您的邮箱为：%s 。请点击此连接激活您的邮箱：</p>' \
                    '<p><a href="%s">%s</p>' % (to_email, verify_url, verify_url)
 
-    send_mail(subject, '', settings.EMAIL_FROM, [to_email], html_message=html_message)
+    try:
+        send_mail(subject, '', settings.EMAIL_FROM, [to_email], html_message=html_message)
+    except Exception as e:
+        logger.error(e)
+        # 有异常自动重试三次
+        raise self.retry(exc=e, max_retries=3)
