47f3dd8ac35c1e2c374b6b8e7d7e20fb8bd3c593
zhouwei1997 <hengyu19971023@163.com>
2023-04-20 21:20:06 +0800

    用户中心-省市区三级联动-缓存

--- meiduo_mall/apps/areas/views.py
+++ meiduo_mall/apps/areas/views.py
@@ -2,6 +2,7 @@
 import logging
 
 from django import http
+from django.core.cache import cache
 from django.views import View
 
 from areas.models import Area
@@ -21,54 +22,62 @@ class AreaView(View):
         """
         area_id = request.GET.get('area_id')
         if not area_id:
-            # 提供省份数据
-            try:
-                province_model_list = Area.objects.filter(parent_isnull=True)
-                # 序列化省级数据
-                province_list = []
-                for province_model in province_model_list:
-                    province_list.append({
-                        'id': province_model.id,
-                        'name': province_model.name
+            # 获取并判断是否有缓存
+            province_list = cache.get('province_list')
+            if not province_list:
+                # 提供省份数据
+                try:
+                    province_model_list = Area.objects.filter(parent_isnull=True)
+                    # 序列化省级数据
+                    province_list = []
+                    for province_model in province_model_list:
+                        province_list.append({
+                            'id': province_model.id,
+                            'name': province_model.name
+                        })
+                    # 缓存省份字典列表数据
+                    cache.set('province_list', province_list, 3600)
+                except Exception as e:
+                    logger.error(e)
+                    return http.JsonResponse({
+                        'code': RETCODE.DBERR,
+                        'errmsg': '省份数据错误'
                     })
-                # 响应省份数据
-                return http.JsonResponse({
-                    'code': RETCODE.OK,
-                    'errmsg': 'OK',
-                    'province_list': province_list
-                })
-            except Exception as e:
-                logger.error(e)
-                return http.JsonResponse({
-                    'code': RETCODE.DBERR,
-                    'errmsg': '省份数据错误'
-                })
+                    # 响应省份数据
+            return http.JsonResponse({
+                'code': RETCODE.OK,
+                'errmsg': 'OK',
+                'province_list': province_list
+            })
         else:
-            try:
-                # 提供市级或区县数据
-                parent_model = Area.objects.get(id=area_id)
-                sub_model_list = parent_model.subs.all()
-                subs = []
-                for sub_model in sub_model_list:
-                    sub_dict = {
-                        "id": sub_model.id,
-                        "name": sub_model.name
+            sub_data = cache.get('sub_data')
+            if not sub_data:
+                try:
+                    # 提供市级或区县数据
+                    parent_model = Area.objects.get(id=area_id)
+                    sub_model_list = parent_model.subs.all()
+                    subs = []
+                    for sub_model in sub_model_list:
+                        sub_dict = {
+                            "id": sub_model.id,
+                            "name": sub_model.name
+                        }
+                        subs.append(sub_dict)
+                    # 子级JSON数据
+                    sub_data = {
+                        'id': parent_model.id,
+                        'name': parent_model.name,
+                        'subs': [{}, {}]
                     }
-                    subs.append(sub_dict)
-                # 子级JSON数据
-                sub_data = {
-                    'id': parent_model.id,
-                    'name': parent_model.name,
-                    'subs': [{}, {}]
-                }
-                return http.JsonResponse({
-                    'code': RETCODE.OK,
-                    'errmsg': 'OK',
-                    'sub_data': sub_data
-                })
-            except Exception as e:
-                logger.error(e)
-                return http.JsonResponse({
-                    'code': RETCODE.DBERR,
-                    'errmsg': '城市或区县数据错误'
-                })
+                    cache.set('sub_data', sub_data, 3600)
+                except Exception as e:
+                    logger.error(e)
+                    return http.JsonResponse({
+                        'code': RETCODE.DBERR,
+                        'errmsg': '城市或区县数据错误'
+                    })
+            return http.JsonResponse({
+                'code': RETCODE.OK,
+                'errmsg': 'OK',
+                'sub_data': sub_data
+            })
