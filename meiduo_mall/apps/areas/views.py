76dbcf530eee2ee4d51135565ce46296d9d22c10
zhouwei1997 <hengyu19971023@163.com>
2023-04-20 21:00:09 +0800

    用户中心-省市区三级联动-市区逻辑实现

--- meiduo_mall/apps/areas/views.py
+++ meiduo_mall/apps/areas/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 import logging
 
-from django.http import JsonResponse
+from django import http
 from django.views import View
 
 from areas.models import Area
@@ -14,7 +14,11 @@ class AreaView(View):
     """省市区数据"""
 
     def get(self, request):
-        """提供省市区数据"""
+        """
+        提供省市区数据
+        :param request:
+        :return: JSON
+        """
         area_id = request.GET.get('area_id')
         if not area_id:
             # 提供省份数据
@@ -28,18 +32,43 @@ class AreaView(View):
                         'name': province_model.name
                     })
                 # 响应省份数据
-                return JsonResponse({
+                return http.JsonResponse({
                     'code': RETCODE.OK,
                     'errmsg': 'OK',
                     'province_list': province_list
                 })
             except Exception as e:
                 logger.error(e)
-                return JsonResponse({
+                return http.JsonResponse({
                     'code': RETCODE.DBERR,
                     'errmsg': '省份数据错误'
                 })
         else:
-            # 提供市级数据
-            pass
-
+            try:
+                # 提供市级或区县数据
+                parent_model = Area.objects.get(id=area_id)
+                sub_model_list = parent_model.subs.all()
+                subs = []
+                for sub_model in sub_model_list:
+                    sub_dict = {
+                        "id": sub_model.id,
+                        "name": sub_model.name
+                    }
+                    subs.append(sub_dict)
+                # 子级JSON数据
+                sub_data = {
+                    'id': parent_model.id,
+                    'name': parent_model.name,
+                    'subs': [{}, {}]
+                }
+                return http.JsonResponse({
+                    'code': RETCODE.OK,
+                    'errmsg': 'OK',
+                    'sub_data': sub_data
+                })
+            except Exception as e:
+                logger.error(e)
+                return http.JsonResponse({
+                    'code': RETCODE.DBERR,
+                    'errmsg': '城市或区县数据错误'
+                })
